53e8d575ac2f14443413f447ba8ba7ddad70ab49
PapriKey <69516652+PapriKey@users.noreply.github.com>
2020-12-04 20:45:06 +0800

    Create 04.cpp

---
+++ Charpter_02/04.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int ye;
+	cout << "Enter your age: ";
+	cin >> ye;
+	cout << "Months: " << ye * 12;
+	//根据ANSI/SIO C++标准，main()函数可省略return 0;
+}
