712a36227cd60c731fd185a74f0bd3d5ae26c198
PapriKey <69516652+PapriKey@users.noreply.github.com>
2020-12-04 20:34:09 +0800

    Create 01.cpp

---
+++ Charpter_02/01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "My name is PapriKey.\n"
+		 << "I live in Github, I'am super like this place!!!";
+	//根据ANSI/SIO C++标准，main()函数可省略return 0;
+}
